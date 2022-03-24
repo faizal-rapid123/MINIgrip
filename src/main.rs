1a6c804b8ee3e868942f38ab1432070df0648e82
faizal khan <faizal@rapidinnovation.dev>
2022-03-24 15:18:20 +0530

    comment add

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,44 @@
-use std::env;
 use std::fs;
+use std::env;
 use std::process;
-
-struct Config 
+struct Config
 {
-    query: String,
-    filename: String,
-}
+    query:String,
+    filename:String
 
+}
 impl Config
 {
-    fn new(args : &[String])->Result<Config, &'static str>
-    {
-        if args.len()<3
-        {
-            return Err("enter both, query and filename");
-        }
-    let query  = args[1].clone();
-    let filename = args[2].clone();
+   fn new (args:&[String])->Result<Config, & 'static str>
+   {
+       if args.len()<3
+       {
+           Err("enter whole query")
+       }
+       else
+       {
+           let query = args[1].clone(); // ownership will be lost so we are using clone method
+           let filename = args[2].clone();
+          return  Ok(Config{query,filename});
+       }
+   }
 
-      Ok(Config{query,filename})
-    }
 }
 
+fn main()
+{
+    let args: Vec<String> = env::args().collect(); // collect information from user
+    let config = Config::new(&args).unwrap_or_else(|err|
+        {
+       println!("Problem parsing arguments: {}", err);
+       process::exit(1);
+   });
 
-fn main() {
+   println!("Searching for {}", config.query);
+   println!("In file {}", config.filename);
 
-        let args: Vec<String> = env::args().collect();
-      
-    
-        let config = Config::new(&args).unwrap_or_else(|err|
-             {
-            println!("Problem parsing arguments: {}", err);
-            process::exit(1);
-        });
-    
-        println!("Searching for {}", config.query);
-        println!("In file {}", config.filename);
-    
-        let contents = fs::read_to_string(config.filename)
-            .expect("Something went wrong reading the file");
- 
-    }
-    
-   
-    
-   
+   let contents = fs::read_to_string(config.filename)
+       .expect("Something went wrong reading the file");
+
+}
+
